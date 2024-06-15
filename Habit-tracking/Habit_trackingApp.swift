fa5748141c7740a9f831e761dd07573e64329632
IrinaZherebtsova <irinazherebtsova1994@gmail.com>
2024-06-15 13:52:33 -1000

    Initial Commit

---
+++ Habit-tracking/Habit_trackingApp.swift
@@ -0,0 +1,17 @@
+//
+//  Habit_trackingApp.swift
+//  Habit-tracking
+//
+//  Created by Irina Zherebtsova on 6/15/24.
+//
+
+import SwiftUI
+
+@main
+struct Habit_trackingApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
